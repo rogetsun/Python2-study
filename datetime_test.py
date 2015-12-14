d5dd0f226704f93fecdd0ce5ad0c599270777a77
rogetsun <songyw163@163.com>
2015-12-14 18:02:14 +0800

    c

--- datetime_test.py
+++ datetime_test.py
@@ -1,6 +1,7 @@
 __author__ = 'uv2sun'
 from datetime import datetime
 import time
+import struct
 
 print time.time()
 
@@ -17,3 +18,7 @@ print time.localtime(time.mktime(dt.timetuple()))
 t = 1434343434L
 
 print time.localtime(t)
+
+t = time.time()
+b = struct.pack("!I", t)
+print [hex(i) for i in struct.unpack("!BBBB", b)]
