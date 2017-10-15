dc2d4d75d5fc81bd571551943e6619e056706f18
rogetsun <songyw163@163.com>
2017-10-15 11:28:16 +0800

    commit

--- find_delete_me_weixin.py
+++ find_delete_me_weixin.py
@@ -28,8 +28,8 @@ import thread
 
 DEBUG = False
 
-MAX_GROUP_NUM = 35  # 每组人数
-INTERFACE_CALLING_INTERVAL = 20  # 接口调用时间间隔, 间隔太短容易出现"操作太频繁", 会被限制操作半小时左右
+MAX_GROUP_NUM = 100  # 每组人数
+INTERFACE_CALLING_INTERVAL = 60  # 接口调用时间间隔, 间隔太短容易出现"操作太频繁", 会被限制操作半小时左右
 MAX_PROGRESS_LEN = 50
 
 QRImagePath = os.path.join(os.getcwd(), 'qrcode.jpg')
